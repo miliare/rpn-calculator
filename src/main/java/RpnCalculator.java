6fa681c77a809edbd4f9c8caf7513e14f6f22739
Rémy Milia <milia.remy@gmail.com>
2018-08-01 11:42:08 +0200

    Fix simple operand ADD the simplest way

--- src/main/java/RpnCalculator.java
+++ src/main/java/RpnCalculator.java
@@ -3,6 +3,12 @@ import static java.lang.Integer.parseInt;
 class RpnCalculator {
 
     int compute(String rpnExpression) {
+        if (rpnExpression.length() > 1) {
+            String[] operands = rpnExpression.split(" ");
+            int leftOperand = parseInt(operands[0]);
+            int rightOperand = parseInt(operands[1]);
+            return leftOperand + rightOperand;
+        }
         return parseInt(rpnExpression);
     }
 }
